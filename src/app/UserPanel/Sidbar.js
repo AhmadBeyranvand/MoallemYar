156de5b2b65084f4caddfc8e9733e22c7278f27d
mr.rahmati <mr.rahmati.9105@gmail.com>
2023-09-24 16:25:10 +0330

    Skeletonization of the project structure

--- src/app/UserPanel/Sidbar.js
+++ src/app/UserPanel/Sidbar.js
@@ -1,10 +1,11 @@
+import Style from "@/app/UserPanel/StyleSidbar.module.css"
+import Logo from '@/assets/logo/logoWhite.svg'
+
 const Sidbar = () => {
     return(
 
         <>
-
-        <p>djksdjksdj</p>
-        
+                
         </>
     )
 }
