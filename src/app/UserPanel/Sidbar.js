82d9735e218ddef5aac47445581c75f1cd398ecb
mr.rahmati <mr.rahmati.9105@gmail.com>
2023-09-25 15:30:50 +0330

    fixd bug

--- src/app/UserPanel/Sidbar.js
+++ src/app/UserPanel/Sidbar.js
@@ -18,59 +18,77 @@ import LogoUser2 from "../../../public/assets/icon/user2.svg"
 const Sidbar = () => {
     return (
         <>
-        <div className="flex">
-            <aside className={Style.aside}>
-                <Image width={250} src={Logo} alt="logo" className="mx-auto p-5" />
-                <div className="pr-12 py-5">
-                    <a className={Style.active}> 
-                        <Image src={LogoHome} alt="home" />
-                        پیشخوان
-                    </a>
+            <div className="flex">
+                <aside className={Style.aside}>
+                    <Image width={250} src={Logo} alt="logo" className="mx-auto p-5" />
+                    <div className="pr-12 py-5">
+                        <div className={Style.active}>
+                                <i>
+                                    <Image src={LogoHome} alt="home" />
+                                </i>
+                            <a href="#" className="flex items-center gap-4">
+                                پیشخوان
+                            </a>
+                        </div>
 
-                    <legend className="py-3">امتحانات</legend>
-                    <ul className="flex flex-col gap-5">
-                        <li className="flex gap-4 items-cente">
-                            <Image src={LogoCalendar} alt="calendar" />
-                            <a className="hover:text-[#F2F2F2] transition-all duration-300 ease">امتحان جدید</a>
-                        </li>
-                        <li className="flex gap-4 items-center">
-                            <Image src={LogoCheck} alt="check" />
-                            <a className="text-[#F2F2F2] hover:text-[#F2F2F2] transition-all duration-300 ease">امتحانات برگزار شده</a>
-                        </li>
-                        <li className="flex gap-4 items-center">
-                            <Image src={LogoClock} alt="clock" />
-                            <a className="hover:text-[#F2F2F2] transition-all duration-300 ease">امتحانات فعال</a>
-                        </li>
-                    </ul>
-                    <legend className="py-3">دانش آموزان</legend>
-                    <ul className="flex flex-col gap-5">
-                        <li className="flex gap-4 items-center">
-                            <Image src={LogoUser} alt="user" />
-                            <a className="hover:text-[#F2F2F2] transition-all duration-300 ease">ثبت دانش آموز جدید</a>
-                        </li>
-                        <li className="flex gap-4 items-center">
-                            <Image src={LogoUsers} alt="users" />
-                            <a className="hover:text-[#F2F2F2] transition-all duration-300 ease">لیست دانش آموزان</a>
-                        </li>
-                    </ul>
-                    <legend className="py-3">کلاس ها</legend>
-                    <div className="gap-5">
-                        <li className="flex gap-4 items-center">
-                            <Image src={LogoClandarUsers} alt="calendarusers" />
-                            <a className="hover:text-[#F2F2F2] transition-all duration-300 ease">مدیریت کلاس ها</a>
-                        </li>
+                        <legend className="py-3">امتحانات</legend>
+                        <ul className="flex flex-col gap-5">
+                            <li className="flex gap-4 items-cente">
+                                <i>
+                                    <Image src={LogoCalendar} alt="calendar" />
+                                </i>
+                                <a href="3" className="hover:text-[#F2F2F2] transition-all duration-300 ease">امتحان جدید</a>
+                            </li>
+                            <li className="flex gap-4 items-center">
+                                <i>
+                                    <Image src={LogoCheck} alt="check" />
+                                </i>
+                                <a href="3" className="text-[#A69FFF] hover:text-[#F2F2F2] transition-all duration-300 ease">امتحانات برگزار شده</a>
+                            </li>
+                            <li className="flex gap-4 items-center">
+                                <i>
+                                    <Image src={LogoClock} alt="clock" />
+                                </i>
+                                <a href="#" className="hover:text-[#F2F2F2] transition-all duration-300 ease">امتحانات فعال</a>
+                            </li>
+                        </ul>
+                        <legend className="py-3">دانش آموزان</legend>
+                        <ul className="flex flex-col gap-5">
+                            <li className="flex gap-4 items-center">
+                                <i>
+                                    <Image src={LogoUser} alt="user" />
+                                </i>
+                                <a href="#" className="hover:text-[#F2F2F2] transition-all duration-300 ease">ثبت دانش آموز جدید</a>
+                            </li>
+                            <li className="flex gap-4 items-center">
+                                <i>
+                                    <Image src={LogoUsers} alt="users" />
+                                </i>
+                                <a href="#" className="hover:text-[#F2F2F2] transition-all duration-300 ease">لیست دانش آموزان</a>
+                            </li>
+                        </ul>
+                        <legend className="py-3">کلاس ها</legend>
+                        <div className="gap-5">
+                            <li className="flex gap-4 items-center">
+                                <i>
+                                    <Image src={LogoClandarUsers} alt="calendarusers" />
+                                </i>
+                                <a href="#" className="hover:text-[#F2F2F2] transition-all duration-300 ease">مدیریت کلاس ها</a>
+                            </li>
+                        </div>
+                        <legend className="py-3">پروفایل کاربری</legend>
+                        <div className="gap-5">
+                            <li className="flex gap-4 items-center">
+                                <i>
+                                    <Image src={LogoUser2} alt="user2" />
+                                </i>
+                                <a href="#" className="hover:text-[#F2F2F2] transition-all duration-300 ease">پروفایل کاربری</a>
+                            </li>
+                        </div>
                     </div>
-                    <legend className="py-3">پروفایل کاربری</legend>
-                    <div className="gap-5">
-                        <li className="flex gap-4 items-center">
-                            <Image src={LogoUser2} alt="user2" />
-                            <a className="hover:text-[#F2F2F2] transition-all duration-300 ease">پروفایل کاربری</a>
-                        </li>
-                    </div>
-                </div>
-            </aside>
-            <div className={Style.radius}></div>
-        </div>
+                </aside>
+                <div className={Style.radius}></div>
+            </div>
         </>
     )
 }
