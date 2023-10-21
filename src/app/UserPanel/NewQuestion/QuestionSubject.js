ba532e8f8e60e9a74759d99987bc161c618a477e
fatemeh <fatemehkhoshnod91@gmail.com>
2023-10-21 11:59:12 +0330

    fixed input

--- src/app/UserPanel/NewQuestion/QuestionSubject.js
+++ src/app/UserPanel/NewQuestion/QuestionSubject.js
@@ -1,7 +1,16 @@
-const QuestionSubject =() =>{
-    return(
-        <div>
-            <input></input>
+
+import React from 'react';
+import { UserOutlined } from '@ant-design/icons';
+import { Input } from 'antd';
+const QuestionSubject = () => {
+    return (
+        <div className='w-full h-screen'>
+            <p>عنوان سوال</p>
+            <div className='flex gap-2 mt-2'> 
+         <Input size="large" placeholder="...اینجاچیزی بنویسید" prefix={<UserOutlined />} />
+          <input  placeholder='برای مثال:2'  prefix={<UserOutlined />}/>
+            </div>
+        
         </div>
     )
 }
