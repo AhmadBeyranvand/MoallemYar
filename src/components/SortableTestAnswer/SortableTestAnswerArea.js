f72bf8604d1c20845f4a3a2c24dd380864e6bbd4
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-23 16:42:32 +0330

    Truncated SortableTestAnswerArea

--- src/components/SortableTestAnswer/SortableTestAnswerArea.js
+++ src/components/SortableTestAnswer/SortableTestAnswerArea.js
@@ -1,8 +1,7 @@
-import React from "react";
-import { Input, Button, Space } from "antd";
+
 export default function SortableTestAnswer() {
   return (
-    <div className="w-7/12 flex justify-center   ">
+    <div className="w-7/12  ">
       
     </div>
   );
