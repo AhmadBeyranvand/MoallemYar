0c947d4613f5b64ff47ba14fe2a51ec5d7355315
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-23 17:03:29 +0330

    set Config Provider for component

--- src/components/SortableTestAnswer/SortableTestAnswerArea.js
+++ src/components/SortableTestAnswer/SortableTestAnswerArea.js
@@ -1,8 +1,21 @@
+import React from "react";
+import { Input, Button, ConfigProvider } from "antd";
 
 export default function SortableTestAnswer() {
   return (
-    <div className="w-7/12  ">
-      
+    <ConfigProvider
+      theme={{
+        token:{
+          colorFill:"#40189E"
+        }
+      }}>
+    <div className="w-7/12 flex justify-center items-center ">
+      <Input placeholder="تهران " />
+      <Input placeholder="ایران " />
+      <Button type="primary">
+        ثبت
+      </Button>
     </div>
+    </ConfigProvider>
   );
 }
