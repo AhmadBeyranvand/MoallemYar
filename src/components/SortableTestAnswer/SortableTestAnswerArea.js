62a64b247d2a21a7b6b35e9eeeb870adc5f2a535
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-23 16:19:58 +0330

    Responsived list of Exams with FHD Resolotion

--- src/components/SortableTestAnswer/SortableTestAnswerArea.js
+++ src/components/SortableTestAnswer/SortableTestAnswerArea.js
@@ -1,8 +1,61 @@
-export default function SortableTestAnswer(){
-    return(
-        <div className="w-7/12 flex flex-col border-solid border-2 border-red-700">
-            SortableTestAnswer
+import React from "react";
+import { Input, Button, Space } from "antd";
+export default function SortableTestAnswer() {
+  return (
+    <div className="w-7/12 flex justify-center   ">
+      <div className="flex flex-col m-2">
+        <Input placeholder="تهران" className="m-1 rounded-none" />
+        <Input placeholder="تهران" className="m-1 rounded-none" />
+        <Input placeholder="تهران" className="m-1 rounded-none" />
       </div>
-    )
+      <div className="flex flex-col m-2">
+        <div className="flex justify-center items-center">
+          <Input placeholder="ایران" className="m-1 rounded-none" />
+          <Button
+            className="underline px-8  mx-1 rounded-none text-sm"
+            type="primary"
+            ghost
+          >
+            ویرایش
+          </Button>
+          <Button
+            className="underline px-5  mx-1 rounded-none text-sm"
+            type="primary"
+            danger
+          >
+            حذف
+          </Button>
+        </div>
+        <div className="flex justify-center items-center">
+          <Input placeholder="ایران" className="m-1 rounded-none" />
+          <Button
+            className="underline px-6 mx-1 rounded-none text-sm"
+            type="primary"
+            ghost
+          >
+            ثبت
+          </Button>
+          <Button
+            className="underline px-6 mx-1 rounded-none text-sm"
+            type="primary"
+            danger
+          >
+            حذف
+          </Button>
+        </div>
+        <div className="flex justify-center items-center">
+          <Input placeholder="ایران" className="m-1 rounded-none" />
 
-    }
+          <Button type="primary">ثبت</Button>
+          <Button
+            className="underline px-4 py-0 mx-1 rounded-none text-sm text-gray-400 border-gray-300"
+            type="text"
+          >
+            ثبت
+          </Button>
+          <Button  primary>primary</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
