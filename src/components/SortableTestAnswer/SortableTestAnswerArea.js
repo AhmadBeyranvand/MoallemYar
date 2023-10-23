171063ea2524027317833e8be7f7220e77fee6e9
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-23 17:41:53 +0330

    Add new option Button and Set Styles it

--- src/components/SortableTestAnswer/SortableTestAnswerArea.js
+++ src/components/SortableTestAnswer/SortableTestAnswerArea.js
@@ -10,14 +10,18 @@ export default function SortableTestAnswer() {
         },
       }}
     >
-      <div className="w-7/12 flex justify-center items-center ">
+      <div className="w-7/12 flex flex-col justify-center items-center ">
         <div className="w-5/6 flex justify-center items-center">
           <Input placeholder="تهران " className=" ml-2 rounded-none" />
           <Input placeholder="ایران " className=" mx-2 rounded-none" />
-          <Button type="primary" className="px-7 rounded-none">ثبت</Button>
+          <Button type="primary" className="px-7 rounded-none">
+            ثبت
+          </Button>
         </div>
         <div className="">
-
+          <Button type="primary" className="px-20 mt-2 rounded-none justify-center items-center">
+            + افزودن گزینه جدید
+          </Button>
         </div>
       </div>
     </ConfigProvider>
