80e693b45bcd51ddaf1fa2f871200e82b195817a
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-23 17:30:49 +0330

    Add input to SortableTestAnswerArea and set styles

--- src/components/SortableTestAnswer/SortableTestAnswerArea.js
+++ src/components/SortableTestAnswer/SortableTestAnswerArea.js
@@ -5,17 +5,21 @@ export default function SortableTestAnswer() {
   return (
     <ConfigProvider
       theme={{
-        token:{
-          colorFill:"#40189E"
-        }
-      }}>
-    <div className="w-7/12 flex justify-center items-center ">
-      <Input placeholder="تهران " />
-      <Input placeholder="ایران " />
-      <Button type="primary">
-        ثبت
-      </Button>
-    </div>
+        token: {
+          colorFill: "#40189E",
+        },
+      }}
+    >
+      <div className="w-7/12 flex justify-center items-center ">
+        <div className="w-5/6 flex justify-center items-center">
+          <Input placeholder="تهران " className=" ml-2 rounded-none" />
+          <Input placeholder="ایران " className=" mx-2 rounded-none" />
+          <Button type="primary" className="px-7 rounded-none">ثبت</Button>
+        </div>
+        <div className="">
+
+        </div>
+      </div>
     </ConfigProvider>
   );
 }
